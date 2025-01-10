f7ebfbd5d1bdd6260dcf879879589eed8d7fc287
Evelyn.Kim <eve0204eve@gmail.com>
2025-01-10 15:21:26 +0900

    삭제 파일 원복

--- JS/index.js
+++ JS/index.js
@@ -8,16 +8,6 @@ document.querySelector('.btn-antiman').addEventListener('click', function (event
 });
 
 // page-3 swiper-slide
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
-
 // document.addEventListener("DOMContentLoaded", function () {
 //   const imgContainer = document.querySelector(".img-container");
 //   const profileList = document.querySelector(".profile-list");
